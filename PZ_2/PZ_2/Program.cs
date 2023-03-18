67e7e5132b119e597148a6681f5e805c24363f04
3pk2BatrutdinovTimur <94544774+TimurBatrutdinov@users.noreply.github.com>
2023-03-18 14:56:18 +0500

    Add files via upload

---
+++ PZ_2/PZ_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+using System.Diagnostics;
+
+bool[,] M = new bool[4, 4]
+{
+    {false, true, false, true}, // G4
+    {true, false, false, false},
+    {true, true, false, true},
+    {false, true, false, false}
+};
+
+Graph graph = new Graph(4, M);
+graph.Depth(1);
+
+public class Graph
+{
+    private int size; // Вершины
+    private bool[,] adjacency; // Матрица
+    private bool[] vector; // Вектор
+    public int Size { get; set; }
+    public bool[,] Adjacency { get; set; }
+    public bool[] Vector { get; set; }
+
+    public Graph(int size, bool[,] G) // Конструктор 
+    {
+        Adjacency = new bool[size, size]; // Инициализация матрицы
+        Adjacency = G;
+        Vector = new bool[size];
+        for (int i = 0; i < size; i++)
+            Vector[i] = false; // Инициализация вектора
+        Size = size;
+    }
+    public void Depth(int i) //i Начало (Вершина)
+    {
+        Vector[i] = true;
+        Console.Write("{0}" + ' ', i);
+        for (int k = 0; k < Size; k++) // Поиск первой 
+            if (Adjacency[i, k] && !(Vector[k]))
+                Depth(k); // Переход к k
+    }
+}
+
+
