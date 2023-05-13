129b2d378ae0e547a9651ed1c1806fa68f4c8b1d
3pk2BatrutdinovTimur <94544774+TimurBatrutdinov@users.noreply.github.com>
2023-05-13 13:59:47 +0500

    Add files via upload

---
+++ PZ_8/PZ_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PZ_8
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _student = ContactDataFactory.CreateContactData("Students") as Students;
+            _student.Name = "Григорян Эди";
+            _student.PhoneNumber = "89123456789";
+            _student.Email = "perviymail.com";
+
+            var _spec = ContactDataFactory.CreateContactData("Specialization") as Specialization;
+            _spec.Name = "Фамилия Имя";
+            _spec.PhoneNumber = "89987654321";
+            _spec.Email = "vtoroy@mail.com";
+
+            var _college = ContactDataFactory.CreateContactData("College") as College;
+            _college.Name = "ОКСЭИ";
+            _college.PhoneNumber = "555-321-123";
+            _college.Address = "Чкалова";
+
+            var AllContacts = new List<IContact> { _student, _spec, _college };
+
+            foreach (var contact in AllContacts)
+            {
+                Console.WriteLine("\nИмя: " + contact.Name);
+                Console.WriteLine("Телефон: " + contact.PhoneNumber);
+            }
+        }
+    }
+}
